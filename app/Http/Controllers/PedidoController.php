fae2f6f2c21ad977922f083c35adc5e86ddb76d8
IudexSoren <aaronsibajam@gmail.com>
2020-11-23 00:52:31 -0600

    Cambio que no recuerdo del controlador de pedido :V

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -57,26 +57,6 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        /*try {
-            $pedido = new Pedido();
-
-            $pedido->instructions = $request->input('instructions');
-            $pedido->cost_shipping = $request->input('cost_shipping');
-            $pedido->discount = $request->input('discount');
-            $pedido->tax = $request->input('tax');
-            $pedido->subtotal = $request->input('subtotal');
-            $pedido->total = $request->input('total');
-            $pedido->user_id = $request->input('user_id');
-            $pedido->personal_entrega_id = $request->input('personal_entrega_id');
-            $pedido->tipoentrega_id = $request->input('tipoentrega_id');
-            $pedido->estadopedido_id = $request->input('estadopedido_id');
-            $pedido->direccion_id = $request->input('direccion_id');
-
-            $pedido->save();
-            return response()->json($pedido, 200);
-        } catch (Exception $e) {
-            return response()->json($e->getMessage(), 422);
-        }*/
         $validator = Validator::make(
             $request->all(),
             [
@@ -142,7 +122,7 @@ class PedidoController extends Controller
     {
         try {
             $pedido =
-                Pedido::where('id', $id)->with(['user', 'productos', 'estadopedido', 'tipoentrega', 'personalentrega'])->first();
+                Pedido::where('id', $id)->with(['user', 'productos', 'estadopedido', 'tipoentrega', 'personal_entrega'])->first();
 
             $response = $pedido;
             return response()->json($response, 200);
